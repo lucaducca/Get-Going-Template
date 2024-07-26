58ebf611c243dd9ebfdb74d5b0eed0a352fa9bc7
luca <128838177+lucaducca@users.noreply.github.com>
2024-07-26 12:32:31 +0100

    Center mission statement when EO and Calendly blank

--- inputs.js
+++ inputs.js
@@ -3,6 +3,7 @@ Step 7: Connecting EmailOctopus and Calendly to your website
 - Add your EmailOctopus and Calendly links between the quotation marks '' below
 */
 const emailOctopusScript = '<script async src="https://eocampaign1.com/form/dc180a9e-4040-11ef-be95-d31e7313d676.js" data-form="dc180a9e-4040-11ef-be95-d31e7313d676"></script>'
+
 const calendlyLink = 'https://calendly.com'
 
 /*
